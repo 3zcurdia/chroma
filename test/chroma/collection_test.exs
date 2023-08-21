ec1fc06b2fc67b6219857005785afa26f52c62c8
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2023-08-21 11:32:09 -0600

    Add create collection tests

--- test/chroma/collection_test.exs
+++ test/chroma/collection_test.exs
@@ -16,4 +16,18 @@ defmodule Chroma.CollectionTest do
                {:ok, [%Chroma.Collection{id: "1234", name: "test", metadata: %{a: 1}}]}
     end
   end
+
+  test "must create a new collection" do
+    with_mock Req,
+      post: fn _url, _body ->
+        {:ok,
+         %Req.Response{
+           status: 200,
+           body: %{"id" => "1234", "name" => "test", "metadata" => %{a: 1}}
+         }}
+      end do
+      assert Chroma.Collection.create("test", %{a: 1}) ==
+               {:ok, %Chroma.Collection{id: "1234", name: "test", metadata: %{a: 1}}}
+    end
+  end
 end
