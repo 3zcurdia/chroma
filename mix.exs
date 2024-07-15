3873845b94c4f4a824f761591223679ba15d0ebc
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2024-07-15 13:23:51 -0600

    Bump package version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chroma.MixProject do
   def project do
     [
       app: :chroma,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.15",
       description: "A ChromaDB client for Elixir",
       start_permanent: Mix.env() == :prod,
